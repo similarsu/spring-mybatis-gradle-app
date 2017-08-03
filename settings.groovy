d6074454541ab035672eca1e26714dce165b6dde
sutong <821192673@qq.com>
2017-08-03 20:48:18 +0800

    add support for muilt env settings

---
+++ settings.groovy
@@ -0,0 +1,21 @@
+enviroments {
+    dev {
+        jdbc {
+            url =''
+            user = ''
+            password = ''
+        }
+    }
+
+    test {
+        url =''
+        user = ''
+        password = ''
+    }
+
+    prod {
+        url =''
+        user = ''
+        password = ''
+    }
+}
