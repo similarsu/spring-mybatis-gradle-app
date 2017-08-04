8d860f007ec8097e1d05931eff79d8d563f89938
sutong <821192673@qq.com>
2017-08-04 09:06:57 +0800

    modify settings.groovy

--- settings.groovy
+++ settings.groovy
@@ -1,21 +1,28 @@
-enviroments {
+environments {
     dev {
         jdbc {
-            url =''
-            user = ''
-            password = ''
+            driver = 'com.mysql.jdbc.Driver'
+            url ='jdbc:mysql://work.kydee.cn:3306/jdasst_dev?useUnicode=true&amp;characterEncoding=utf8'
+            user = 'jdasst_dev'
+            password = 'jdasst_dev'
         }
     }
 
     test {
-        url =''
-        user = ''
-        password = ''
+        jdbc {
+            driver = 'com.mysql.jdbc.Driver'
+            url ='jdbc:mysql://work.kydee.cn:3306/jdasst_test?useUnicode=true&amp;characterEncoding=utf8'
+            user = 'jdasst_test'
+            password = 'jdasst_test'
+        }
     }
 
     prod {
-        url =''
-        user = ''
-        password = ''
+        jdbc {
+            driver = 'com.mysql.jdbc.Driver'
+            url ='jdbc:mysql://work.kydee.cn:3306/jdasst_prod?useUnicode=true&amp;characterEncoding=utf8'
+            user = 'jdasst_prod'
+            password = 'jdasst_prod'
+        }
     }
 }
